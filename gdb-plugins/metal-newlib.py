1fc032cc0405ef9c58d19a65bfb5293084794b9c
Klemens David Morgenstern <klemens.morgenstern@gmx.net>
2020-01-29 23:03:50 +0700

    updated newlib script (bunch of fixes)

--- gdb-plugins/metal-newlib.py
+++ gdb-plugins/metal-newlib.py
@@ -2,150 +2,142 @@ import traceback
 
 import gdb
 import os
+import stat
 
 
 
 class open_flags:
 
     def __init__(self):
-        try: self.o_append   = int(str(gdb.parse_and_eva("O_APPEND")  ))
-        except: pass
-        try: self.o_creat    = int(str(gdb.parse_and_eva("O_CREAT")   ))
-        except: pass
-        try: self.o_excl     = int(str(gdb.parse_and_eva("O_EXCL")    ))
-        except: pass
-        try: self.o_noctty   = int(str(gdb.parse_and_eva("O_NOCTTY")  ))
-        except: pass
-        try: self.o_nonblock = int(str(gdb.parse_and_eva("O_NONBLOCK")))
-        except: pass
-        try: self.o_sync     = int(str(gdb.parse_and_eva("O_SYNC")    ))
-        except: pass
-        try: self.o_async    = int(str(gdb.parse_and_eva("O_ASYNC")   ))
-        except: pass
-        try: self.o_cloexec  = int(str(gdb.parse_and_eva("O_CLOEXEC") ))
-        except: pass
-        try: self.o_direct   = int(str(gdb.parse_and_eva("O_DIRECT")  ))
-        except: pass
-        try: self.o_directory= int(str(gdb.parse_and_eva("O_DIRECTORY")))
-        except: pass
-        try: self.o_dsync    = int(str(gdb.parse_and_eva("O_DSYNC")   ))
-        except: pass
-        try: self.o_largefile= int(str(gdb.parse_and_eva("O_LARGEFILE")))
-        except: pass
-        try: self.o_noatime  = int(str(gdb.parse_and_eva("O_NOATIME") ))
-        except: pass
-        try: self.o_ndelay   = int(str(gdb.parse_and_eva("O_NDELAY")  ))
-        except: pass
-        try: self.o_path     = int(str(gdb.parse_and_eva("O_PATH")    ))
-        except: pass
-        try: self.o_trunc    = int(str(gdb.parse_and_eva("O_TRUNC")   ))
-        except: pass
-        try: self.o_rdonly   = int(str(gdb.parse_and_eva("O_RDONLY")  ))
-        except: pass
-        try: self.o_wronly   = int(str(gdb.parse_and_eva("O_WRONLY")  ))
-        except: pass
-        try: self.o_rdwr     = int(str(gdb.parse_and_eva("O_RDWR")    ))
-        except: pass
-        try: self.s_iread    = int(str(gdb.parse_and_eva("S_IREAD")   ))
-        except: pass
-        try: self.s_iwrite   = int(str(gdb.parse_and_eva("S_IWRITE")  ))
-        except: pass
-        try: self.s_iwusr    = int(str(gdb.parse_and_eva("S_IWUSR")   ))
-        except: pass
-        try: self.s_ixusr    = int(str(gdb.parse_and_eva("S_IXUSR")   ))
-        except: pass
-        try: self.s_irwxg    = int(str(gdb.parse_and_eva("S_IRWXG")   ))
-        except: pass
-        try: self.s_irgrp    = int(str(gdb.parse_and_eva("S_IRGRP")   ))
-        except: pass
-        try: self.s_iwgrp    = int(str(gdb.parse_and_eva("S_IWGRP")   ))
-        except: pass
-        try: self.s_ixgrp    = int(str(gdb.parse_and_eva("S_IXGRP")   ))
-        except: pass
-        try: self.s_irwxo    = int(str(gdb.parse_and_eva("S_IRWXO")   ))
-        except: pass
-        try: self.s_iroth    = int(str(gdb.parse_and_eva("S_IROTH")   ))
-        except: pass
-        try: self.s_iwoth    = int(str(gdb.parse_and_eva("S_IWOTH")   ))
-        except: pass
-        try: self.s_ixoth    = int(str(gdb.parse_and_eva("S_IXOTH")   ))
-        except: pass
-        try: self.s_isuid    = int(str(gdb.parse_and_eva("S_ISUID")   ))
-        except: pass
-        try: self.s_isgid    = int(str(gdb.parse_and_eva("S_ISGID")   ))
-        except: pass
-        try: self.s_isvtx    = int(str(gdb.parse_and_eva("S_ISVTX")   ))
-        except: pass
+        self.o_append    = 0x0008
+        self.o_creat     = 0x0200
+        self.o_excl      = 0x0800
+        self.o_noctty    = 0x8000
+        self.o_nonblock  = 0x4000
+        self.o_sync      = 0x2000
+        self.o_async     = 0x0040
+        self.o_cloexec   = 0x40000
+        self.o_direct    = 0x80000
+        self.o_directory = 0x200000
+        self.o_dsync     = 0
+        self.o_largefile = 0
+        self.o_noatime   = 0
+        self.o_ndelay    = 0
+        self.o_path      = 0
+        self.o_trunc     = 0x0400
+        self.o_rdonly    = 0
+        self.o_wronly    = 1
+        self.o_rdwr      = 2
+        self.s_iread     = 0o000400
+        self.s_iwrite    = 0o000200
+        self.s_iwusr     = 0o000200
+        self.s_ixusr     = 0o000100
+        self.s_irgrp     = 0o000040
+        self.s_iwgrp     = 0o000020
+        self.s_ixgrp     = 0o000010
+        self.s_irwxg     = self.s_irgrp | self.s_iwgrp | self.s_ixgrp
+        self.s_iroth     = 0o000004
+        self.s_iwoth     = 0o000002
+        self.s_ixoth     = 0o000001
+        self.s_irwxo     = self.s_iroth | self.s_iwoth | self.s_ixoth
+        self.s_isuid     = 0o004000
+        self.s_isgid     = 0o002000
+        self.s_isvtx     = 0o001000
+        self.s_irusr     = 0o000400
+        self.s_irwxu     = self.s_irusr | self.s_iwusr | self.s_ixusr
+
 
     def get_flags(self, in_):
 
         out = 0
-        out |= os.O_BINARY
+        try:
+            out |= os.O_BINARY
+        except:
+            pass
         if in_ & self.o_append   : out |= os.O_APPEND
         if in_ & self.o_creat    : out |= os.O_CREAT
         if in_ & self.o_excl     : out |= os.O_EXCL
         if in_ & self.o_rdonly   : out |= os.O_RDONLY
         if in_ & self.o_wronly   : out |= os.O_WRONLY
         if in_ & self.o_rdwr     : out |= os.O_RDWR
-        if in_ & self.o_noctty   : out |= os.O_NOCTTY
-        if in_ & self.o_nonblock : out |= os.O_NONBLOCK
-        if in_ & self.o_sync     : out |= os.O_SYNC
-        if in_ & self.o_async    : out |= os.O_ASYNC
-        if in_ & self.o_cloexec  : out |= os.O_CLOEXEC
-        if in_ & self.o_direct   : out |= os.O_DIRECT
-        if in_ & self.o_directory: out |= os.O_DIRECTORY
-        if in_ & self.o_dsync    : out |= os.O_DSYNC
-        if in_ & self.o_largefile: out |= os.O_LARGEFILE
-        if in_ & self.o_noatime  : out |= os.O_NOATIME
-        if in_ & self.o_ndelay   : out |= os.O_NDELAY
-        if in_ & self.o_path     : out |= os.O_PATH
+        try:
+            if in_ & self.o_noctty   : out |= os.O_NOCTTY
+        except: pass
+        try:
+            if in_ & self.o_nonblock : out |= os.O_NONBLOCK
+        except: pass
+        try:
+            if in_ & self.o_sync     : out |= os.O_SYNC
+        except: pass
+        try:
+            if in_ & self.o_async    : out |= os.O_ASYNC
+        except: pass
+        try:
+            if in_ & self.o_cloexec  : out |= os.O_CLOEXEC
+        except: pass
+        try:
+            if in_ & self.o_direct   : out |= os.O_DIRECT
+        except: pass
+        try:
+            if in_ & self.o_directory: out |= os.O_DIRECTORY
+        except: pass
+        try:
+            if in_ & self.o_dsync    : out |= os.O_DSYNC
+        except: pass
+        try:
+            if in_ & self.o_largefile: out |= os.O_LARGEFILE
+        except: pass
+        try:
+            if in_ & self.o_noatime  : out |= os.O_NOATIME
+        except: pass
+        try:
+            if in_ & self.o_ndelay   : out |= os.O_NDELAY
+        except: pass
+        try:
+            if in_ & self.o_path     : out |= os.O_PATH
+        except: pass
         if in_ & self.o_trunc    : out |= os.O_TRUNC
-
         return out
 
 
     def get_mode(self, in_):
 
         out = 0
-        if in_ & self.s_irwxu:   out |= os.S_IREAD | os.S_IWRITE
-        if in_ & self.s_irusr:   out |= os.S_IREAD
-        if in_ & self.s_iwusr:   out |= os.S_IWRITE
-        if in_ & self.s_iread:   out |= os.S_IREAD
-        if in_ & self.s_iwrite:  out |= os.S_IWRITE
-        if in_ & self.s_irwxu:   out |= os.S_IRWXU
-        if in_ & self.s_irusr:   out |= os.S_IRUSR
-        if in_ & self.s_iwusr:   out |= os.S_IWUSR
-        if in_ & self.s_ixusr:   out |= os.S_IXUSR
-        if in_ & self.s_irwxg:   out |= os.S_IRWXG
-        if in_ & self.s_irgrp:   out |= os.S_IRGRP
-        if in_ & self.s_iwgrp:   out |= os.S_IWGRP
-        if in_ & self.s_ixgrp:   out |= os.S_IXGRP
-        if in_ & self.s_irwxo:   out |= os.S_IRWXO
-        if in_ & self.s_iroth:   out |= os.S_IROTH
-        if in_ & self.s_iwoth:   out |= os.S_IWOTH
-        if in_ & self.s_ixoth:   out |= os.S_IXOTH
-        if in_ & self.s_isuid:   out |= os.S_ISUID
-        if in_ & self.s_isgid:   out |= os.S_ISGID
-        if in_ & self.s_isvtx:   out |= os.S_ISVTX
+        if in_ & self.s_irwxu:   out |= stat.S_IREAD | stat.S_IWRITE
+        if in_ & self.s_iwusr:   out |= stat.S_IWRITE
+        if in_ & self.s_iread:   out |= stat.S_IREAD
+        if in_ & self.s_iwrite:  out |= stat.S_IWRITE
+        if in_ & self.s_irwxu:   out |= stat.S_IRWXU
+        if in_ & self.s_irusr:   out |= stat.S_IRUSR
+        if in_ & self.s_iwusr:   out |= stat.S_IWUSR
+        if in_ & self.s_ixusr:   out |= stat.S_IXUSR
+        if in_ & self.s_irwxg:   out |= stat.S_IRWXG
+        if in_ & self.s_irgrp:   out |= stat.S_IRGRP
+        if in_ & self.s_iwgrp:   out |= stat.S_IWGRP
+        if in_ & self.s_ixgrp:   out |= stat.S_IXGRP
+        if in_ & self.s_irwxo:   out |= stat.S_IRWXO
+        if in_ & self.s_iroth:   out |= stat.S_IROTH
+        if in_ & self.s_iwoth:   out |= stat.S_IWOTH
+        if in_ & self.s_ixoth:   out |= stat.S_IXOTH
+        if in_ & self.s_isuid:   out |= stat.S_ISUID
+        if in_ & self.s_isgid:   out |= stat.S_ISGID
+        if in_ & self.s_isvtx:   out |= stat.S_ISVTX
         return out
 
 class seek_flags:
 
     def __init__(self):
-        try: self.seek_set = int(str(gdb.parse_and_eval("SEEK_SET")));
-        except: pass
-        try: self.seek_cur = int(str(gdb.parse_and_eval("SEEK_CUR")));
-        except: pass
-        try: self.seek_end = int(str(gdb.parse_and_eval("SEEK_END")));
-        except: pass
+        self.seek_set = 0
+        self.seek_cur = 1
+        self.seek_end = 2
 
     def get_flags(self, in_):
-        out = 0;
-        if in_ & self.seek_set: out |= os.SEEK_SET;
-        if in_ & self.seek_cur: out |= os.SEEK_CUR;
-        if in_ & self.seek_end: out |= os.SEEK_END;
+        out = 0
+        if in_ & self.seek_set: out |= os.SEEK_SET
+        if in_ & self.seek_cur: out |= os.SEEK_CUR
+        if in_ & self.seek_end: out |= os.SEEK_END
 
-        return out;
+        return out
 
 class metal_test_backend(gdb.Breakpoint):
     def __init__(self):
@@ -158,16 +150,14 @@ class metal_test_backend(gdb.Breakpoint):
         try:
             fr = gdb.selected_frame()
             args = [arg for arg in fr.block() if arg.is_argument]
-
-            type = str(args[0].value())
-            oper = str(args[1].value(fr))[len("metal_func_"):]
+            oper = str(args[0].value(fr))[len("metal_func_"):]
 
             try:
                 getattr(self, oper)(args, fr)
             except OSError as e:
                 gdb.execute("set var errno = {}".format(e.errno))
             except Exception as e:
-                gdb.write("Internal error {}\n".format(e), gdb.STDERR)
+                gdb.write("Internal error [{}] {}\n".format(oper, e), gdb.STDERR)
 
             return False
         except gdb.error as e:
@@ -176,12 +166,16 @@ class metal_test_backend(gdb.Breakpoint):
             raise e
 
     def close(self, args, frame):
-        fd = int(str(args[3].value()))
-        res = os.close(fd)
-        gdb.execute("return {}".format(res))
+        fd = int(str(args[3].value(frame)))
+        try:
+            os.close(fd)
+            gdb.execute("return 0")
+        except:
+            gdb.execute("return -1")
+            raise
 
     def fstat(self, args, frame):
-        fd = int(str(args[3].value()))
+        fd = int(str(args[3].value(frame)))
         try:
             st = os.fstat(fd)
             gdb.execute("set_var")
@@ -203,20 +197,20 @@ class metal_test_backend(gdb.Breakpoint):
             gdb.execute("return {}".format(e.errno))
 
     def isattay(self, args, frame):
-        fd = int(str(args[3].value()))
+        fd = int(str(args[3].value(frame)))
         gdb.execute("return {}".format(os.isatty(fd)))
 
     def link(self, args, frame):
-        fd = int(str(args[3].value()))
-        existing = str(args[1].value())
-        _new = str(args[2].value())
+        fd = int(str(args[3].value(frame)))
+        existing = str(args[1].value(frame))
+        _new = str(args[2].value(frame))
         ret = os.link(existing, _new)
         gdb.execute("return {}".format(ret))
 
     def lseek(self, args, frame):
-        fd = int(str(args[3].value()))
-        ptr = int(str(args[4].value()))
-        dir_in = int(str(args[5].value()))
+        fd = int(str(args[3].value(frame)))
+        ptr = int(str(args[4].value(frame)).split(' ')[0], 0)
+        dir_in = int(str(args[5].value(frame)))
 
         if self.seek_flags is None:
             self.seek_flags = seek_flags()
@@ -226,22 +220,27 @@ class metal_test_backend(gdb.Breakpoint):
         gdb.execute("return {}".format(ret))
 
     def open(self, args, frame):
-        file = str(args[1].value())
-        flags_in = int(str(args[3].value()))
-        mode_in = int(str(args[3].value()))
+        file = str(args[1].value(frame).string())
+        flags_in = int(str(args[3].value(frame)))
+        mode_in = int(str(args[3].value(frame)))
+
 
         if self.open_flags is None:
-            self.open_flags = open_flags
+            self.open_flags = open_flags()
 
         flags = self.open_flags.get_flags(flags_in)
-        mode  = self.open_flags.get_mode (mode_in)
+        mode = self.open_flags.get_mode(mode_in)
+
+        if flags & os.O_CREAT:
+            mode |= stat.S_IRUSR | stat.S_IRGRP | stat.S_IRWXO
+
         fd = os.open(file, flags, mode)
         gdb.execute("return {}".format(fd))
 
     def read(self, args, frame):
-        fd = int(str(args[3].value()))
-        len = int(str(args[4].value()))
-        ptr = int(str(args[6].value()))
+        fd = int(str(args[3].value(frame)))
+        len = int(str(args[4].value(frame)))
+        ptr = int(str(args[6].value(frame)).split(' ')[0], 0)
 
         buf = os.read(fd, len)
 
@@ -249,7 +248,7 @@ class metal_test_backend(gdb.Breakpoint):
         gdb.execute("return {}".format(len(buf)))
 
     def stats(self, args, frame):
-        file = str(args[1].value())
+        file = str(args[1].value(frame))
         st = os.stat(file)
 
         gdb.execute("set var arg7->st_dev   ={}".format(st.st_dev))
@@ -267,22 +266,25 @@ class metal_test_backend(gdb.Breakpoint):
         gdb.execute("return 0")
 
     def symblink(self, args, frame):
-        existing = str(args[1].value())
-        _new = str(args[2].value())
+        existing = str(args[1].value(frame))
+        _new = str(args[2].value(frame))
 
         ret = os.symlink(existing, _new)
         gdb.execute("return {}".format(ret))
 
     def unlink(self, args, frame):
-        name = str(args[6].value())
+        name = str(args[6].value(frame))
         ret = os.unlink(name)
         gdb.execute("return {}".format(ret))
 
     def write(self, args, frame):
-        fd = int(str(args[3].value()))
-        len = int(str(args[4].value()))
-        ptr = int(str(args[6].value()))
-
+        fd = int(str(args[3].value(frame)))
+        len = int(str(args[4].value(frame)))
+        ptr = int(str(args[6].value(frame)).split(' ')[0], 0)
         buf = gdb.selected_inferior().read_memory(ptr, len)
-        ret = os.write(fd, buf)
+        ret = os.write(fd, str(buf))
         gdb.execute("return {}".format(ret))
+
+
+
+mtb = metal_test_backend()
